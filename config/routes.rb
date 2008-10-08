8225b801712ae6ffa91c13f3dc385e87ca4f37bf
robmckinnon <rob@theyworkforyou.co.nz>
2008-10-08 15:59:27 +0100

    added make_resourceful as a git submodule; added constituency editing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
 
+  map.resources :constituencies
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "postcodes"
   map.connect '/postcodes/:postcode.:format', :controller => "postcodes", :action => 'show'
