0579c811da3d380f758a55801394e912733638c5
robmckinnon <rob@theyworkforyou.co.nz>
2008-10-08 14:35:50 +0100

    fixed spec failures

--- app/controllers/postcodes_controller.rb
+++ app/controllers/postcodes_controller.rb
@@ -19,18 +19,27 @@ class PostcodesController < ApplicationController
     end
   end
 
+  def toggle_admin
+    if request.post?
+      session[:is_admin] = !session[:is_admin]
+    end
+    redirect_to :back
+  end
+
   def show
     code = params[:postcode]
-    postcode = Postcode.find_by_code(code, :include => {:constituency => :member})
+    postcode = Postcode.find_postcode_by_code(code)
 
     unless postcode
-      postcode = Postcode.find_by_code(code.tr(' ',''))
+      postcode = Postcode.find_postcode_by_code(code.tr(' ',''))
       if postcode
         redirect_to :action=>'show', :postcode=>postcode.code
+      else
+        flash[:not_found] = "Postcode #{code} not found." if code
+        params[:postcode] = nil
+        redirect_to :action=>'index'
       end
-    end
-
-    if postcode
+    else
       respond_to do |format|
         format.html { @postcode = postcode }
         format.xml  { @postcode = postcode }
@@ -40,11 +49,6 @@ class PostcodesController < ApplicationController
         format.csv { render :text => postcode.to_csv }
         format.yaml { render :text => postcode.to_output_yaml }
       end
-    else
-      flash[:not_found] = "Postcode #{code} not found." if code
-
-      params[:postcode] = nil
-      redirect_to :action=>'index'
     end
   end
 end
