350beac5c223835ccc6278759187e7c2b5ffe8ea
robmckinnon <rob@theyworkforyou.co.nz>
2008-10-17 17:37:10 +0100

    redirect to index if constituencies route requested and user is not admin

--- app/controllers/constituencies_controller.rb
+++ app/controllers/constituencies_controller.rb
@@ -1,2 +1,11 @@
 class ConstituenciesController < ResourceController::Base
+
+  before_filter :redirect_if_not_admin
+
+  def redirect_if_not_admin
+    unless is_admin?
+      redirect_to :controller => 'postcodes', :action => 'index'
+    end
+  end
+
 end
