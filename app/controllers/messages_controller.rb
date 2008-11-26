7de7d2c3a65e87374a629ea32059faa868e36b7f
robmckinnon <rob@theyworkforyou.co.nz>
2008-11-26 16:40:31 +0000

    redirect to index page any attempt to access a sent message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,9 +6,22 @@ class MessagesController < ResourceController::Base
 
   # before_filter :redirect_if_not_admin, :except => ['new','create','show','edit']
 
+  before_filter :redirect_if_message_sent, :except => ['new']
+
   def redirect_if_not_admin
     unless is_admin?
       redirect_to :controller => 'postcodes', :action => 'index'
+    end
+  end
+
+  def redirect_if_message_sent
+    if params[:constituency_id] && params[:id]
+      if Constituency.exists?(params[:constituency_id])
+        message = Message.find_by_constituency_id_and_id(params[:constituency_id], params[:id])
+        if message.sent
+          redirect_to :controller => 'postcodes', :action => 'index'
+        end
+      end
     end
   end
 
