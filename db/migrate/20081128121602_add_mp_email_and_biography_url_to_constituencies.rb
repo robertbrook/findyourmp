e33cee2113b9fc8a28d93ab544aabfbf1da7d1ba
robmckinnon <rob@theyworkforyou.co.nz>
2008-11-28 12:24:08 +0000

    added mp email, bio url and visible flag to constituencies

---
+++ db/migrate/20081128121602_add_mp_email_and_biography_url_to_constituencies.rb
@@ -0,0 +1,13 @@
+class AddMpEmailAndBiographyUrlToConstituencies < ActiveRecord::Migration
+  def self.up
+    add_column :constituencies, :member_email, :string
+    add_column :constituencies, :member_biography_url, :string
+    add_column :constituencies, :member_visible, :boolean
+  end
+
+  def self.down
+    remove_column :constituencies, :member_email
+    remove_column :constituencies, :member_biography_url
+    remove_column :constituencies, :member_visible
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081110144031) do
+ActiveRecord::Schema.define(:version => 20081128121602) do
 
   create_table "constituencies", :force => true do |t|
-    t.string "name"
-    t.string "member_name"
+    t.string  "name"
+    t.string  "member_name"
+    t.string  "member_email"
+    t.string  "member_biography_url"
+    t.boolean "member_visible"
   end
 
   create_table "messages", :force => true do |t|
@@ -29,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20081110144031) do
     t.time     "sent_on"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "authenticity_token"
   end
 
   create_table "postcodes", :force => true do |t|
