68dfae74424cb669aaa1810ecedad091ffdc7b56
Liz <Liz@liz-conlans-macbook.local>
2008-12-22 16:16:45 +0000

    more features stuff

---
+++ features/step_definitions/setup.rb
@@ -0,0 +1,15 @@
+Before do
+  Given 'there is a postcode "AB101AA" in constituency "Aberdeen North"'
+  Given 'there is an MP "Frank Doran" in constituency "Aberdeen North"'
+  
+  Given 'there is a postcode "ML14BW" in constituency "Motherwell and Wishaw"'
+  Given 'there is an MP "Mr Frank Roy" in constituency "Motherwell and Wishaw"'
+  
+  Given 'there is a postcode "AB101BE" in constituency "Aberdeen South"'
+  Given 'there is an MP "Miss Anne Begg" in constituency "Aberdeen South"'
+  
+  Given 'there is a postcode "KY8 5XY" in constituency "Glenrothes"'
+  Given 'there is no MP in constituency "Glenrothes"'
+
+  Given 'there is a postcode "GY1 1AB" with no constituency'
+end
