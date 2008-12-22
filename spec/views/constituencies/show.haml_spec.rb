584cf4bf6646449421a2f7091d9a8b81952d2bdd
Liz <Liz@liz-conlans-macbook.local>
2008-12-22 17:07:28 +0000

    added spec for constituencies not showing Website: or Biography: labels when no data supplied

--- spec/views/constituencies/show.haml_spec.rb
+++ spec/views/constituencies/show.haml_spec.rb
@@ -11,13 +11,36 @@ describe "/constituencies/show.haml" do
         :member_email => "member@email"
         )
     @constituency.stub!(:name).and_return("MyString")
-
-    assigns[:constituency] = @constituency
+    
+    @other_constituency = mock_model(Constituency, :member_name => 'member_name',
+        :member_party => 'member_party',
+        :member_biography_url => '',
+        :member_website => '',
+        :no_sitting_member? => false,
+        :member_email => "member@email"
+        )
+      @other_constituency.stub!(:name).and_return("MyString")
   end
 
   it "should render attributes in <p>" do
+    assigns[:constituency] = @constituency
+        
     render "/constituencies/show.haml"
     response.should have_text(/MyString/)
+  end
+  
+  it "should not render member website when the data is not supplied" do
+    assigns[:constituency] = @other_constituency
+    
+    render "/constituencies/show.haml"
+    response.should_not have_text(/Website:/)
+  end
+  
+  it "should not render member biography when the data is not supplied" do
+    assigns[:constituency] = @other_constituency
+    
+    render "/constituencies/show.haml"
+    response.should_not have_text(/Biography:/)
   end
 end
 
