88a4e68c4d8116e24f2225f28414d0a4382fd0af
robmckinnon <rob@theyworkforyou.co.nz>
2009-01-21 12:03:42 +0000

    improved rcov coverage

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,13 +2,4 @@ class MembersController < ResourceController::Base
 
   belongs_to :constituency
 
-  protected
-
-  def object
-    parent_object.member
-  end
-
-  def object_url
-    send 'constituency_url', parent_object
-  end
 end
