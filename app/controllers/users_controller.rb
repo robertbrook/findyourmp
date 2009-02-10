09f8fbde66fac61cb42fbef298d3297feb2df76e
robmckinnon <rob@theyworkforyou.co.nz>
2009-02-10 16:31:59 +0000

    improved layout of user admin views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
 
-  before_filter :require_no_user, :only => [:new, :create]
-  before_filter :require_user, :only => [:show, :edit, :update]
+
+  layout 'application'
+  before_filter :require_user
 
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 
   def create
