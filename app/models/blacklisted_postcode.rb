1477c4d035ad1cc367af079760bd96f5253eb819
lizconlan <Liz@liz-conlans-macbook.local>
2009-08-21 14:33:14 +0100

    added restore method to BlacklistedPostcode

--- app/models/blacklisted_postcode.rb
+++ app/models/blacklisted_postcode.rb
@@ -1,4 +1,12 @@
 class BlacklistedPostcode < ActiveRecord::Base
   validates_uniqueness_of :code
   validates_presence_of :ons_id
+  
+  def restore
+    postcode = Postcode.find_by_code code
+    unless postcode
+      postcode = Postcode.create(:code => code, :constituency_id => constituency_id, :ons_id => ons_id)
+    end
+    self.delete if postcode
+  end
 end
--- spec/models/blacklisted_postcode_spec.rb
+++ spec/models/blacklisted_postcode_spec.rb
@@ -1,17 +1,15 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe BlacklistedPostcode do
+  before do
+    @blacklisted_postcode = BlacklistedPostcode.new
+  end
   
   describe 'when asked to find blacklisted postcode by code' do
-    before do
-      @postcode = BlacklistedPostcode.new
-    end
-    
     it 'should return a match' do
       code = 'N12SD'
-      BlacklistedPostcode.should_receive(:find_by_code).with(code).and_return @postcode
-      
-      BlacklistedPostcode.find_by_code(code).should == @postcode
+      BlacklistedPostcode.should_receive(:find_by_code).with(code).and_return @blacklisted_postcode
+      BlacklistedPostcode.find_by_code(code).should == @blacklisted_postcode
     end
     
     it 'should return nil if given nil code' do
@@ -19,4 +17,39 @@ describe BlacklistedPostcode do
     end
   end
   
+  describe 'when asked to restore a blacklisted postcode' do
+    before do
+      @postcode = mock_model(Postcode)
+      @ons_id = 444
+      @constituency_id = 5
+      @code = 'N215SD'
+      @blacklisted_postcode.stub!(:ons_id).and_return @ons_id
+      @blacklisted_postcode.stub!(:constituency_id).and_return @constituency_id
+      @blacklisted_postcoed.stub!(:code).and_return @code
+    end
+    
+    it 'should insert a new Postcode entry if none already exists' do
+      Postcode.should_receive(:find_by_code).and_return nil
+      Postcode.should_receive(:create).and_return @postcode
+      @blacklisted_postcode.should_receive(:delete)
+      
+      @blacklisted_postcode.restore
+    end
+    
+    it 'should not attempt to create a new Postcode entry if one already exists' do
+      Postcode.should_receive(:find_by_code).and_return @postcode
+      Postcode.should_not_receive(:create)
+      @blacklisted_postcode.should_receive(:delete)
+      
+      @blacklisted_postcode.restore
+    end
+    
+    it 'should not delete the BlacklistedPostcode if the creation of a new Postcode fails' do
+      Postcode.should_receive(:find_by_code).and_return nil
+      Postcode.should_receive(:create).and_return nil
+      @blacklisted_postcode.should_not_receive(:delete)
+      
+      @blacklisted_postcode.restore
+    end
+  end
 end
