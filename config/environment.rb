8ac23ef8f7ecf255a6e52f0ea21eda9c4a8edc06
lizconlan <lizconlan@gmail.com>
2013-04-05 16:29:24 +0100

    updated environment files

--- config/environment.rb
+++ config/environment.rb
@@ -1,86 +1,12 @@
-# Be sure to restart your server when you modify this file
+Encoding.default_external = Encoding.default_internal = Encoding::UTF_8
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+# Load the rails application
+require File.expand_path('../application', __FILE__)
 
-# Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
+# Initialize the rails application
+FindYourMP::Application.initialize!
 
-Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "aws-s3", :lib => "aws/s3"
-  # config.gem 'ar-extensions'
-  # config.gem "adzap-ar_mailer", :lib => 'action_mailer/ar_mailer', :source => 'http://gems.github.com'
-
-  # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  # Skip frameworks you're not going to use. To use Rails without a database,
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'UTC'
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  config.autoload_paths << "#{RAILS_ROOT}/app/sweepers"
-
-  # Force all environments to use the same logger level
-  # (by default production uses :info, the others :debug)
-  # config.log_level = :debug
-
-  # Your secret key for verifying cookie session data integrity.
-  # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random,
-  # no regular words or you'll be exposed to dictionary attacks.
-  config.action_controller.session = {
-    :key => '_findyourmp_session',
-    :secret      => '46530d6a3d7dcbdd1a8d6935314dbd52fe689e773e15cd833a09cac0fb68d797fcc0d1b8202f58ccf643ada161aa1e2ea48801b0b6bce7b0b9b0e99ddecdcbbd'
-  }
-
-  # Use the database for sessions instead of the cookie-based default,
-  # which shouldn't be used to store highly confidential information
-  # (create the session table with "rake db:sessions:create")
-  # config.action_controller.session_store = :active_record_store
-
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
-
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector
-end
-
-require 'passenger_mgt'
-require 'route_helper'
-require 'sitemap'
-require 'zlib'
-
-Haml::Template.options[:format] = :html5
-
-ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
-  :month_year => "%B %Y"
-)
-ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
-  :month_year => "%B %Y"
-)
-
-ActiveRecord::Base.send(:include, CountByMonth)
-ActiveRecord::Base.send(:include, FindYourMP::PassengerManagement)
-
+require 'haml'
+require 'authlogic'
+require 'friendly_id'
+require 'dynamic_form'
